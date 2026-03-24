70db1d455f5430e7e26ce66c3e3920e94bf43e6a
Hanna <hannamuzychuk@gmail.com>
2026-03-24 14:30:47 +0100

    add middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import dotenv from 'dotenv';
+import { logger } from './middleware/logger.js';
+import cors from 'cors';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
+
+app.use(logger);
+app.use(express.json);
+app.use(cors());
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 await connectMongoDB();
 
