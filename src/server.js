d914a4fae7ca20641a7e2e40a3a35961385faeab
Hanna <hannamuzychuk@gmail.com>
2026-03-24 21:16:01 +0100

    add changes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { logger } from './middleware/logger.js';
 import cors from 'cors';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './roures/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 dotenv.config();
 
