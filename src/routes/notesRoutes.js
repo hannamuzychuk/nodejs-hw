8521ce3fb4af7f3e29b168d4f56922ab221d9a70
Hanna <hannamuzychuk@gmail.com>
2026-03-31 13:50:16 +0200

    feat: add validation to createNote

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
-import { getAllNotesSchema, noteIdSchema } from "../validations/notesValidation.js";
+import { getAllNotesSchema, noteIdSchema, createNoteSchema } from "../validations/notesValidation.js";
 import { celebrate } from "celebrate";
 
 const router = Router();
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', createNote);
+router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', deleteNote);
 router.patch('/notes/:noteId', updateNote);
 
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -3,11 +3,11 @@ import { TAGS } from "../constants/tags.js";
 import { isValidObjectId } from "mongoose";
 
 export const getAllNotesSchema = {
-   [Segments.QUERY]: Joi.object()({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().allow(''),
+   [Segments.QUERY]: Joi.object({
+       page: Joi.number().integer().min(1).default(1),
+       perPage: Joi.number().integer().min(5).max(20).default(10),
+       tag: Joi.string().valid(...TAGS),
+       search: Joi.string().allow(''),
    }),
 };
 
@@ -19,5 +19,13 @@ export const noteIdSchema = {
             }
             return value;
         }),
+    }),
+};
+
+export const createNoteSchema = {
+    [Segments.BODY]: Joi.object({
+        title: Joi.string().min(1).required(),
+        content: Joi.string().allow(''),
+        tag: Joi.string().valid(...TAGS),
     }),
 };
