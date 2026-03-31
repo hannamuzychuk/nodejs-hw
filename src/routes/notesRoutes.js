1ba35ac079f54fc844118863015116edf47ca8ce
Hanna <hannamuzychuk@gmail.com>
2026-03-31 14:00:06 +0200

    feat: update noteRoute

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
