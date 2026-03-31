f6db2594e79de5c5612ca1e72816ff263d77ba4b
Hanna <hannamuzychuk@gmail.com>
2026-03-31 13:34:58 +0200

    feat: add validation to all notes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
+import { getAllNotesSchema } from "../validations/notesValidation.js";
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
 router.delete('/notes/:noteId', deleteNote);
